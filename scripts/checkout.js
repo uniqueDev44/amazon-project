c24e2f00ee3f2c8c29ffa2fcb0e7698442ba99f5
uniqueDev44 <uniquedev44@gmail.com>
2025-11-02 08:38:42 -0800

    use container.remove to remove cartItem from the page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,7 @@ cart.forEach((cartItem) => {
     }
   })
   matchingProductHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -95,9 +95,10 @@ document.querySelector('.js-order-summary')
 const deleteLink = document.querySelectorAll('.js-delete-quantity-link');
 deleteLink.forEach((link) => {
   link.addEventListener('click', () => {
-    const productId = link.dataset.productId
+    const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart)
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove()
   })
 })
 
