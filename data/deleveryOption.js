e241bb5032e87fd18ba3024b4391834f9ca55bd5
uniqueDev44 <uniquedev44@gmail.com>
2025-11-03 19:37:46 -0800

    create a delevery option array and store the delevery option data

---
+++ data/deleveryOption.js
@@ -0,0 +1,17 @@
+export const deleveryOptions = [
+  {
+    id: '1',
+    deleveryDays: 7,
+    priceCents: 0
+  },
+  {
+    id: '2',
+    deleveryDays: 3,
+    priceCents: 499
+  }, 
+  {
+    id: '3',
+    deleveryDays: 1,
+    priceCents: 999
+  }
+]
