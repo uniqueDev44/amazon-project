be73b5fab7487d0df9a447cab997ffcbb368eb5a
uniqueDev44 <uniquedev44@gmail.com>
2025-11-06 21:47:27 -0800

    function that render checkout header html with javascript
    1. create a function that renders checkoutHtml
    i2. n the function loop through cart and calculate the cart quantity
    3.  after the loop create a variable and save the checkout header html
    4.save the calculation variable inside the quantity label html
    5. get the dom and save the variable
    6 export the function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
 
